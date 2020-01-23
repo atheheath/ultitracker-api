eaaceb4d810cd1b3523aad01590efe1526fa689f
Alex Heath <alexander.t.heath@gmail.com>
2020-01-23 00:36:45 -0600

    Move to docker compose for dev environment

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cp -r ~/.aws ./.aws
-docker build --tag ultitrackerapi-dev --file dev.dockerfile .
+
+# do this to export variables
+# we get from sourcing
+set -a
+source ./config/dev.env
+
+# start clean everytime
+docker-compose --file docker-compose-dev.yml down --volumes
+docker-compose --file docker-compose-dev.yml build
+# docker build --tag ultitrackerapi-dev --file dev.dockerfile .
 rm -rf ./.aws
