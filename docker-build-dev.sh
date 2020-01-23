4c3cda0b35be3b63bf85af4cf528073ad1d9b58e
Alex Heath <alexander.t.heath@gmail.com>
2020-01-23 00:37:31 -0600

    Missed save

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -8,6 +8,7 @@ source ./config/dev.env
 
 # start clean everytime
 docker-compose --file docker-compose-dev.yml down --volumes
+
 docker-compose --file docker-compose-dev.yml build
 # docker build --tag ultitrackerapi-dev --file dev.dockerfile .
 rm -rf ./.aws
