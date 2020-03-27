443dc8982dbaa515f397ef1b9b1b6523c72f5490
Alexander Heath <alexander.t.heath@gmail.com>
2020-03-27 00:51:00 -0500

    Update create-environment.sh

--- ultitrackerapi/scripts/elastic-beanstalk/create-environment.sh
+++ ultitrackerapi/scripts/elastic-beanstalk/create-environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws elasticbeanstalk create-environment \
     --application-name ultitracker-api \
-    --environment-name ultitracker-api-https-v0-0-4 \
+    --environment-name ultitracker-api-https-v0-0-5 \
     --tier "Name=WebServer,Type=Standard,Version= " \
     --solution-stack-name "64bit Amazon Linux 2018.03 v2.14.2 running Docker 18.09.9-ce" \
-    --version-label https-v0.0.3 \
+    --version-label https-v0.0.4 \
     --option-settings file://./environment-config-https.json
